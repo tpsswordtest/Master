3f91bc195978c263cf07c15179c51a2b2503f14e
tpsswordtest <tpsswordtest@dontrackme.com>
2020-09-13 10:02:35 +0100

    update test spec to work with cypress

--- cypress/integration/examples/e2e/Test.spec.js
+++ cypress/integration/examples/e2e/Test.spec.js
@@ -1,6 +1,4 @@
-const { CaseStudies } = require("../pageObjects/case-studies/case-studies.page");
-
-import {CaseStudies} from '../pageObjects/case-studies/case-studies.page'
+import {CaseStudies} from '../../../../pageObjects/case-studies/case-studies.page'
 // type definitions for Cypress object "cy"
 /// <reference types="cypress" />
 
@@ -13,10 +11,11 @@ describe('Test Case Study', () => {
   cy.visit(testurl);
   
  //A unique picture is displayed for each case study
- cs.getAllPictures().each(($el) => {
-    // $el is a wrapped jQuery element
-    if ($el) {
-      cy.wrap($el).click()
-    }
-})
+  cs.getAllPictures().each(($el) => {
+      // $el is a wrapped jQuery element
+      if ($el) {
+        cy.wrap($el).click()
+      }
+    })
+  })
 })
